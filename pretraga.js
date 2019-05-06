5c52d526a2beb41456d0febd0661e710096d1832
Ivan Antic <47058987+pera-kojot-genije@users.noreply.github.com>
2019-05-06 22:43:41 +0200

    Add files via upload

--- pretraga.js
+++ pretraga.js
@@ -1,6 +1,47 @@
-
-
 let d = id => document.getElementById(id)
+
+// IZBOR TEME
+
+function izborTeme(tema) {
+  document.getElementById('osnovna-tema').setAttribute('href', tema);
+}
+
+/* PRIJAVI SE */
+
+function loginBox() {
+  const lgn = document.getElementById("login-box");
+  if (lgn.style.display === "block") {
+    lgn.style.display = "none";
+  } else {
+    lgn.style.display = "block";
+  }
+}
+
+const logIn = d('login-box')
+
+logIn.addEventListener('click', function () {
+  if (username.value == "admin" && password.value == "admin") {
+    const iks = document.getElementsByClassName('iks')
+    console.log(iks);
+    for (let i = 0; i < iks.length; i++) {
+      iks[i].style.display = "block"
+    }
+  }
+
+})
+
+// /* NAVIGACIJA */
+
+function hamburger() {
+  const x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// let d = id => document.getElementById(id)
 
 const prikaz = d('prikaz')
 const kriterijum = d('kriterijum')
