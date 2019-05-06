dc5c137e8e19fe5f35461de8010d2f3867291bb4
Ivan Antic <47058987+pera-kojot-genije@users.noreply.github.com>
2019-05-06 22:51:09 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function render(niz) {
   let sablon = ''
   const limit = niz.length >= 8 ? 8 : niz.length
   for (var i = 0; i < limit; i++) {
-    sablon += ` <div class="film">
+    sablon += ` <div class="solo-film">
             <h3 class= "naslov">${niz[i].naziv} </h3> 
             <p>• Godina : ${niz[i].godina}</p><hr>
              <img src=${niz[i].slika} alt="" class="slike">
